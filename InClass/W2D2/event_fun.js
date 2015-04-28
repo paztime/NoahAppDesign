39ef717718b8b5325efc01d5287eb4a5b470da62
Benjamin Ylvisaker <ben.ylvisaker@coloradocollege.edu>
2015-04-28 11:04:45 -0600

    Chair and table

--- InClass/W2D2/event_fun.js
+++ InClass/W2D2/event_fun.js
@@ -1,4 +1,6 @@
 var lastCell;
+var W = 50;
+var H = 50;
 
 function pageLoad()
 {
@@ -7,35 +9,53 @@ function pageLoad()
     {
         var td = tds.item( i );
         td.onclick = mouseClick;
-        td.width = 20;
-        td.height = 20;
+        td.width = W;
+        td.height = H;
     }
     lastCell = document.getElementById( "middle" );
     lastCell.next = null;
+    lastCell.duration = 1000;
+    lastCell.started = true;
+    var img = document.createElement( "img" );
+    lastCell.appendChild( img );
     window.setTimeout( colorYellow, 1000, lastCell );
 }
 
 function colorBlack( elem )
 {
     // console.log( "black" );
-    elem.style.backgroundColor = "black";
-    window.setTimeout( colorYellow, 1000, elem );
-    if( elem.next != null )
+    // elem.style.backgroundColor = "black";
+    elem.started = true;
+    var img = elem.childNodes[ 0 ];
+    img.src = "./Images/chair1.jpeg";
+    img.width=W;
+    img.height=H;
+    window.setTimeout( colorYellow, elem.duration, elem );
+    if( elem.next != null && !elem.next.started )
         window.setTimeout( colorBlack, 100, elem.next );
 }
 
 function colorYellow( elem )
 {
     // console.log( "yellow" );
-    elem.style.backgroundColor = "yellow";
-    window.setTimeout( colorBlack, 1000, elem );
-    if( elem.next != null )
+    //elem.style.backgroundColor = "yellow";
+    elem.started = true;
+    var img = elem.childNodes[ 0 ];
+    img.src = "./Images/table1.jpg";
+    img.width=W;
+    img.height=H;
+    window.setTimeout( colorBlack, elem.duration, elem );
+    if( elem.next != null && !elem.next.started )
         window.setTimeout( colorYellow, 100, elem.next );
 }
 
 function mouseClick()
 {
     // console.log( "click" );
+    var img = document.createElement( "img" );
+    this.appendChild( img );
+    this.started = false;
+    this.duration = lastCell.duration * 2;
     lastCell.next = this;
     lastCell = this;
     this.next = null;
