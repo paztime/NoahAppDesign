e6b0bd18d0e00f1a31f4a562c2c9f5ff8358f918
Benjamin Ylvisaker <ben.ylvisaker@coloradocollege.edu>
2015-04-30 11:35:04 -0600

    hello sql

---
+++ InClass/W2D4/hello_sql.js
@@ -0,0 +1,12 @@
+var sqlite = require( 'sqlite3' );
+
+var db = new sqlite.Database( "telluride.sqlite" );
+
+db.each( "SELECT ID, PERFORMER, STAGE, TIME FROM PERFORMANCE", function( err, row ) {
+    db.each( "SELECT NAME FROM PERFORMERS WHERE ID = " +
+             row.PERFORMER, function( err2, row2 ) {
+                 console.log( "Performer: "+row.PERFORMER + " " +
+                              row2.NAME );
+             });
+    console.log( row );
+});
